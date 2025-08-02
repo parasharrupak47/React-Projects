6183a995a56ef3b9558d70acc9dd8b2ea9140b95
parasharrupak47 <rupakp.ug23.cs@nitp.ac.in>
2025-08-02 16:26:05 +0530

    added bgchanger project

---
+++ bgchanger/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [color , setColor] = useState("olive");
+
+  return (
+    <div className="w-full h-screen duration-200 overflow-hidden"
+    style={{backgroundColor : color}}>
+      <div className="fixed flex flex-wrap justify-center bottom-12
+      inset-x-0 px-2">
+        <div className="flex flex-wrap justify-center gap-3 shadow-lg bg-amber-950
+        px-3 py-2 rounded-3xl">
+          <button className="outline-none px-4 py-1
+           rounded-full shadow-lg" style={{backgroundColor : "red"}}
+           onClick={() => setColor('red')}>Red</button>
+          <button className="outline-none px-4 py-1
+           rounded-full" style={{backgroundColor : "green"}}
+           onClick={() => setColor('green')}>Green</button>
+          <button className="outline-none px-4 py-1
+           rounded-full" style={{backgroundColor : "blue"}}
+           onClick={() => setColor('blue')}>Blue</button>
+          <button className="outline-none px-4 py-1
+           rounded-full" style={{backgroundColor : "yellow"}}
+           onClick={() => setColor('yellow')}>Yellow</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default App
